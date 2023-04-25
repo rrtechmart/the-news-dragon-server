dbf5bc459c0d0245d7f2bc7371b38ba1df48fd75
rrtechmart <reza.cpbd@gmail.com>
2023-04-25 09:56:48 +0600

    added all file

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express();
+const port = 5000;
+
+const categories = require('./data/categories.json')
+
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('Dragon is running ')
+})
+
+app.get('/categories', (req, res)=>{
+    res.send(categories)
+})
+
+app.listen(port, ()=>{
+    console.log(`Dragon is running on port ${port}`)
+})
